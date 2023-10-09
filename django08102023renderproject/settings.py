3cacaa9cdd9d94d513bf4dbe14925f53ea80578b
git90s <90sstudioart@gmail.com>
2023-10-09 13:26:40 +0530

    sh

--- django08102023renderproject/settings.py
+++ django08102023renderproject/settings.py
@@ -91,6 +91,7 @@ else:
                 'ENGINE': 'django.db.backends.sqlite3',
                 'NAME': BASE_DIR / 'db.sqlite3',
             }
+            # "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
         }
 
 
