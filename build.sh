d5b602eb934b1d87efd63561f32f379a1dd1eb75
git90s <90sstudioart@gmail.com>
2023-10-09 13:58:54 +0530

    gfj

--- build.sh
+++ build.sh
@@ -6,10 +6,10 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 
-if [[ $CREATE_SUPERUSER ]]
+if [[ $CREATE_SUPERUSER ]];
 then
     python manage.py createsuperuser --no-input
 fi
-python manage.py makemigrations
+# python manage.py makemigrations
 python manage.py migrate
 
